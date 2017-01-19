e4e2a00f745dcb62e9feddf1bfbae2c8201190a7
Erico MacTavish <Erico MacTavish>
2017-01-19 21:57:15 +0800

    finished imgSlider

---
+++ src/imgSlider.js
@@ -0,0 +1,14 @@
+$(function () {
+    showZone();
+});
+function showZone() {
+    var $next=$('h3');
+    var $slideBanner=$('.slideBanner');
+    $next.click(function () {
+        if($slideBanner.css('left')!='-1600px'){
+            $slideBanner.animate({'left':'-=800px'}, 1000);
+        } else{
+            $slideBanner.animate({'left':'0px'}, 1000);
+        }
+    });
+}
